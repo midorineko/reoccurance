55b0756a9c3256d749511c074d3d04aa2ea1267c
midorineko <steven.inouye91@gmail.com>
2016-06-22 11:46:44 -0700

    calculated the next 5 dates -next we will check if the start date comes before or after the first date

--- truelink/app/models/event.rb
+++ truelink/app/models/event.rb
@@ -4,4 +4,15 @@ class Event < ActiveRecord::Base
 		return Date.parse("#{action_day}-#{start_date.month}-#{start_date.year}");
 	end
 
+	def first_five_actions_dates
+		start_date = first_action_date()
+		action_array = []
+		action_array << start_date
+		4.times do
+			action_array << start_date+interval.months
+			start_date = start_date+interval.months
+		end
+		return action_array
+	end
+
 end
