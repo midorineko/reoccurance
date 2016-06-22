697e3f0f2a8c88e51bd8fc59a888540a04c06bd0
midorineko <steven.inouye91@gmail.com>
2016-06-22 12:06:28 -0700

    I now correctly bring up the next four action dates -I need to toake the 5 calculated dates and make them into action dates

--- truelink/app/models/event.rb
+++ truelink/app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ActiveRecord::Base
 
-	def first_action_date
+	def first_start_date
 		return Date.parse("#{action_day}-#{start_date.month}-#{start_date.year}");
 	end
 
-	def first_five_actions_dates
-		start_date = first_action_date()
+	def first_five_calculated_dates
+		start_date = first_start_date()
 		action_array = []
 		action_array << start_date
 		4.times do
@@ -13,6 +13,16 @@ class Event < ActiveRecord::Base
 			start_date = start_date+interval.months
 		end
 		return action_array
+	end
+
+	def next_four_action_dates
+		first_five = first_five_calculated_dates()
+		if start_date > first_five.first
+			first_five.shift
+		else
+			first_five.pop
+		end
+		return first_five
 	end
 
 end
