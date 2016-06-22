417fa2a0552ab1b747360b779b7fca2250eef8f7
midorineko <steven.inouye91@gmail.com>
2016-06-22 15:37:37 -0700

    fixed tests

--- truelink/spec/controllers/events_controller_spec.rb
+++ truelink/spec/controllers/events_controller_spec.rb
@@ -4,7 +4,7 @@ describe EventsController do
 	render_views
 
   describe "GET Index" do
-  	before {@event = Event.create!(title: 'test', start_date: Date.parse('20-9-2016'), interval: 1, action_day: 10)}
+  	before {@event = Event.create!(title: 'test', start_date: Date.parse('20-9-2016'), interval: 1, action_day: 10, buffer: 2)}
     it "gets the index view" do
       get "index"
       response.status.should be 200
@@ -20,7 +20,7 @@ describe EventsController do
   end
 
   describe "GET Show" do
-  	before {@event = Event.create!(title: 'test', start_date: Date.parse('20-9-2016'), interval: 1, action_day: 10)}
+  	before {@event = Event.create!(title: 'test', start_date: Date.parse('20-9-2016'), interval: 1, action_day: 10, buffer: 2)}
     it "gets the Show view" do
       get :show, id: @event
       response.status.should be 200
